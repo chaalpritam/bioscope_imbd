789afff5f2a2f1cb7f886913ee535347fc54ac58
shyamala <shamu0987654321@gmail.com>
2016-10-11 20:31:58 +0530

    COMPLETED

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Film, Genre, Country, Stars
+admin.site.register(Film)
+admin.site.register(Genre)
+admin.site.register(Country)
+admin.site.register(Stars)
--- films/models.py
+++ films/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from decimal import Decimal
 
-# Create your models here.
+RATING_CHOICES = (
+('G', 'General Audiences - G'),
+('PG', 'Parental Guidance Suggested - PG'),
+('PG-13', 'Parents Strongly Cautioned - PG-13'),
+('R', 'Restricted - R'),
+('NC-17', 'Adults Only - NC-17'),
+)
+
+class Genre(models.Model):
+	genres = models.CharField(max_length=30, unique=True)
+	def __str__(self): 
+		return "%s" % self.genres
+
+class Country(models.Model):
+	country = models.CharField(max_length=60, unique=True)
+	def __str__(self): 
+		return "%s" % self.country
+
+class Stars(models.Model):
+	stars = models.CharField(max_length=40, unique=True)
+	def __str__(self): 
+		return "%s" % self.stars
+
+class Film(models.Model):
+	created_date=models.DateTimeField(auto_now_add=True)
+	modified_date=models.DateTimeField(auto_now=True)
+	film_name=models.CharField(max_length=20,blank=False,unique=True)
+	year=models.IntegerField()
+	rating=models.CharField(max_length=80,choices=RATING_CHOICES,default="G")
+	hours_min=models.IntegerField()
+	genres=models.ManyToManyField(Genre)
+	release_date=models.DateTimeField(auto_now=False)
+	country=models.ManyToManyField(Country)
+	user_rating=models.DecimalField(max_digits=10,decimal_places=1,default=Decimal('0.0'))
+	display_image=models.ImageField(upload_to="display_picture/")
+	banner_image=models.ImageField(upload_to="banner_picture/")
+	trailer_link=models.URLField(blank=True,help_text="Trailer url")
+	description=models.TextField()
+	director=models.CharField(max_length=80,blank=False)
+	writer=models.CharField(max_length=80,blank=False)
+	stars=models.ManyToManyField(Stars)
+	metascore=models.IntegerField()
+	user_review=models.IntegerField()
+	critic_review=models.IntegerField()
+	popularity=models.IntegerField()
+	created_by=models.ForeignKey(User,null=True,blank=True)
+	def __str__(self): 
+		return "%s" % self.film_name
