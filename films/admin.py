ba8043448b9391d83ce40093ee72be21cb7ad140
dinesh <dinesh4u2k@gmail.com>
2016-10-15 22:29:04 +0530

    task_2_completed

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import flim, Genre, country, stars
+admin.site.register(flim)
+admin.site.register(Genre)
+admin.site.register(country)
+admin.site.register(stars)
--- films/views.py
+++ films/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import flim
+
+class HomeView(ListView):
+     model = flim
+     queryset = flim.objects.order_by('-created_by')[:9]
+     template_name = 'index.html'
+
+def get_context_data(self, **kwargs):
+    context = super(homeview,self).get_context_data(**kwargs)
+    return context
