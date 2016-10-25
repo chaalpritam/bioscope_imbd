30e7c18a33fc65745223175c4662ce5c9aeb0a3c
venky <venky@example.com>
2016-10-25 20:24:07 +0530

    completed task 2

--- films/views.py
+++ films/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Film
+
+class HomeView(ListView):
+
+    model = Film
+    queryset = Film.objects.order_by('-created_date')[:9]
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super(HomeView,self).get_context_data(**kwargs)
+        return context
