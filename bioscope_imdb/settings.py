0968f8e10b27b16f7c8b62f7d04a1935edfd457f
tm <thiyaga0513@gmail.com>
2016-12-02 16:44:31 +0530

    completed task2

--- bioscope_imdb/settings.py
+++ bioscope_imdb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bioscope_imdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Film, Genre, Country, Stars
 # Register your models here.
+admin.site.register(Film)
+admin.site.register(Genre)
+admin.site.register(Country)
+admin.site.register(Stars)
