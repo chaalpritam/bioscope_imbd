e2ca985e90b47019f760487a27d0452dd7cdf037
ajithnaruto <ajithnaruto@gmail.com>
2016-12-03 18:47:59 +0530

    completed task 2

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Film,Genre,Country,Stars
+
+
+admin.site.register(Film)
+admin.site.register(Genre)
+admin.site.register(Country)
+admin.site.register(Stars)
 
 # Register your models here.
