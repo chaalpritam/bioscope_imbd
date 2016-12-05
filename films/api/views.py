ef0b698c93809a79af035b79cbcc2f8229f4b0b8
monsteramba <ambalavanankarthic@gmail.com>
2016-12-06 03:13:34 +0530

    TAsk2

---
+++ films/api/views.py
@@ -0,0 +1,20 @@
+from rest_framework.generics import ListAPIView,RetrieveAPIView
+
+from films.models import Film
+
+from .serializers import (
+    FilmListSerializer,
+    FilmDetailSerializer,
+    )
+
+
+class FilmListAPIView(ListAPIView):
+    serializer_class = FilmListSerializer
+    queryset = Film.objects.all()
+
+
+
+class FilmDetailAPIView(RetrieveAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmDetailSerializer
+    lookup_field = "slug"
