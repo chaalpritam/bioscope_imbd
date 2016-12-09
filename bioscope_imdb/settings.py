24d1fe07030dbc0f26f30bc2fab0e117e0b55cf4
ajithnaruto <ajithnaruto@gmail.com>
2016-12-09 20:02:07 +0530

    heroku deploy

--- bioscope_imdb/settings.py
+++ bioscope_imdb/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
 
     )
 }
